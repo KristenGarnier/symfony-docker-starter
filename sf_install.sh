d33221fb398dcbf914cbf0379c809331a2ec27f0
kristengarnier <garnier.kristen@icloud.com>
2019-05-23 16:41:13 +0200

    Update install procedure

--- sf_install.sh
+++ sf_install.sh
@@ -4,7 +4,10 @@ echo "Installation process of symfony 4"
 
 cd /home/wwwroot/sf4
 
-composer create-project symfony/skeleton .
+composer create-project symfony/skeleton install-directory
+
+cp -Rf /home/wwwroot/sf4/install-directory/. .
+rm -Rf /home/wwwroot/sf4/install-directory
 
 echo "Installation process was successfull"
 
